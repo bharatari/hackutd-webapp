0247fcc8fe6ff81788e3681035ab8a607b43d617
John Kahn <jjk150430@utdallas.edu>
2017-03-04 02:12:50 -0600

    Stylized the schedule and added error checking

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,22 +14,24 @@ export default class Sidebar extends React.Component {
   render() {
     let renderedRoutes = [];
     routes.forEach((route, index) => {
-      renderedRoutes.push(<li key="{ index }"><a href="{ route.value }">{ route.key }</a></li>);
+      renderedRoutes.push(<li key={ index }><a href={ route.value }>{ route.key }</a></li>);
     });
 
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <a href="/" className='brand-logo'><img src="http://hackutd.co/img/LogoWhiteHorizontal.svg" /></a>
-          <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-          <ul className="right hide-on-med-and-down">
-            { renderedRoutes }
-          </ul>
-          <ul className="side-nav" id="mobile-demo">
-            { renderedRoutes }
-          </ul>
-        </div>
-      </nav>
+      <div className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper">
+            <a href="/" className='brand-logo'><img src="http://hackutd.co/img/LogoWhiteHorizontal.svg" /></a>
+            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              { renderedRoutes }
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              { renderedRoutes }
+            </ul>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
--- src/routes/Schedule/View.js
+++ src/routes/Schedule/View.js
@@ -12,38 +12,46 @@ export default class ScheduleView extends React.Component {
     this.props.actions.fetchEvents();
   }
   render() {
-  
-    let tempEvents = [
-      {
-        name: 'Registration',
-        start: '9:00am',
-      },
-      {
-        name: 'Opening Ceremony',
-        start: '11:00am',
-        end: '12:00pm'
-      },
-      {
-        name: 'Hacking Begins',
-        start: '12:00pm',
-      },
-      {
-        name: 'Lunch',
-        start: '1:00pm',
-      },
-    ];
+    let showSchedule = () => {
+      return (
+        <div>
+          <div className="container schedule hide-on-small-only">
+            <div className="row">
+              <div className="col s12 l8 offset-l2">
+                <h3>Schedule</h3>
+                <List data={this.props.events ? this.props.events : null} />
+              </div>
+            </div>
+          </div>
+          <div className="schedule hide-on-med-and-up">
+            <List data={this.props.events ? this.props.events : null} />
+          </div>
+        </div>
+      );
+    };
+
+    let showError = () => {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col s12 l8 offset-l2">
+              <h3 className="hide-on-small-only">Schedule</h3>
+              <div className="card">
+                <div className="card-content">
+                  <span className="card-title center-align"><i className="material-icons">warning</i></span>
+                  <p className="center-align">There was an error displaying the schedule</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    };
 
     return (
       <div>
         <Navbar />
-        <div className="container schedule">
-          <div className="row">
-            <div className="col s12 l8 offset-l2">
-              <h3>Schedule</h3>
-              <List data={this.props.events ? this.props.events : tempEvents} />
-            </div>
-          </div>
-        </div>
+        {this.props.events ? showSchedule() : showError()}
       </div>
     );
   }
